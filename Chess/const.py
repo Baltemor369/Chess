4b88ebbe8de77334b8d1a66b73d1111ef139caa7
Baltemor369 <118623957+Baltemor369@users.noreply.github.com>
2023-10-03 11:04:33 +0200

    add some colors
    for more visual understanding and clearness

--- Chess/const.py
+++ Chess/const.py
@@ -1,14 +1,25 @@
 # color 
+WHITEEGG = (210,210,210)
 WHITE = (255,255,255)
 BLACK = (0,0,0)
-GRAYLIGHT = (66,66,66)
-GREENLIGHT = (50,225,50)
-BLUELIGHT = (55,55,225)
+GREENLIGHT = (70,170,70)
+# selected pîece
+YELLOWLIGHT = (255,255,0)
+YELLOWDARK = (150,150,0)
+# move color
+BLUELIGHT = (120,120,225)
+BLUEDARK = (70,70,180)
+# in check color
 REDLIGHT = (225,55,55)
+# default bg
+WOOD = (139, 69, 19)
+
 # size var
 CASE_SIZE = 50
 IMG_SIZE = 32
 SCREEN_H = 800
 SCREEN_W = 800
+
 # other
 MAX_LOG = 30
+PROMOTE_CHOICE = ["queen","rook","knight","bishop"]
