4cf8e35df861c5b05e3b3bb77a65ee995a412844
Baltemor369 <118623957+Baltemor369@users.noreply.github.com>
2023-10-11 22:22:41 +0200

    update the main file

--- Chess/main.py
+++ Chess/main.py
@@ -1,10 +1,5 @@
 from Chess import Chess
 
-# TODO:
-# check King move
-# add counter moves done by each player
-# double step for pawn
-
 if __name__ == '__main__':
-    chess = Chess("black")
+    chess = Chess()
     chess.run()
